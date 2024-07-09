02c3d5e42c6e7df88b19c9a23c85e008bf7f5115
norlab-warthog <norlabwarthog@gmail.com>
2024-07-09 16:29:49 -0400

    change launch file to call the 3 right comm port  by [DF]

--- gpsx/launch/emlid.launch.py
+++ gpsx/launch/emlid.launch.py
@@ -9,14 +9,14 @@ def generate_launch_description():
             package='gpsx',
             executable='gps_node',
             name='emlid_E845', 
-            parameters=[{'comm_port': "/dev/emlidE845"}, {'comm_speed': 9600}],
+            parameters=[{'comm_port': "/dev/ttyS0"}, {'comm_speed': 115200}],
             remappings= [('/gpsx', 'topic_emlid_E845')]
         ),
         Node(
             package='gpsx',
             executable='gps_node',
             name='emlid_6802', 
-            parameters=[{'comm_port': "/dev/emlid6802"}, {'comm_speed': 9600}],
+            parameters=[{'comm_port': "/dev/ttyS1"}, {'comm_speed': 115200}],
             remappings= [('/gpsx', 'topic_emlid_6802')]
         ),
 
@@ -24,7 +24,7 @@ def generate_launch_description():
             package='gpsx',
             executable='gps_node',
             name='emlid_C959', 
-            parameters=[{'comm_port': "/dev/emlidC959"}, {'comm_speed': 9600}],
+            parameters=[{'comm_port': "/dev/ttyS2"}, {'comm_speed': 115200}],
             remappings= [('/gpsx', 'topic_emlid_C959')]
         ),
         
